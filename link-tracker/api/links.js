0bf689f936c6b0f6858e71e06f6adb7c71a6edb7
monparasahil440-jpg <monparasahil440-jpg@users.noreply.github.com>
2026-07-13 21:22:47 +0530

    test links api

--- link-tracker/api/links.js
+++ link-tracker/api/links.js
@@ -1,15 +1,5 @@
-const { getLinks, getClicks, saveLinks, saveClicks } = require('./_storage');
-
-module.exports = async (req, res) => {
-  if (req.method === 'GET') {
-    return res.status(200).json(getLinks());
-  }
-
-  if (req.method === 'DELETE') {
-    saveLinks({});
-    saveClicks({});
-    return res.json({ ok: true });
-  }
-
-  res.status(405).json({ error: 'Method Not Allowed' });
+module.exports = (req, res) => {
+  res.status(200).json({
+    test: "links api works"
+  });
 };
